b346bab82bb6962654b3b8e2c5487f6a79cdae7e
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-11 12:04:25 -0300

    chore: absolute paths and dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 module.exports = {
 
     entry: './app/src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: './app/dist'
+        path: path.resolve(__dirname, 'app/dist')
     }
 }
 
-// executado na plataforma node
+// executado na plataforma nodejs
 //todos os scripts são gerados em único bundle, o único que será carregado no navegador
+// o path exige um caminho absoluto. __dirname indica o diretório atual em que o arquivo de config se encontra 
+    // indica todo o caminho desde a raiz de qualquer SO até a pasta dist
