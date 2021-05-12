741aa20a6d2f96b8347da8fc4555a08f405ad813
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-12 17:23:05 -0300

    chore: minifying css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
 
@@ -17,6 +18,10 @@ module.exports = {
                 use: [ MiniCssExtractPlugin.loader,'css-loader'] 
             }
         ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [ new CssMinimizerWebpackPlugin()]
     },
     plugins: [
         new HtmlWebpackPlugin({
